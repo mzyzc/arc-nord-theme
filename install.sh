665ee85a63d010d770276745c86b05ed642b9c4c
mzyzc <mzyzc@czyz.xyz>
2021-01-22 09:06:12 +0000

    Added an install script

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
-./autogen.sh --prefix=/usr --disable-transparency --disable-light --disable-darker --disable-cinnamon --disable-gnome-shell --disable-metacity --disable-plank --disable-unity --disable-xfwm
+./autogen.sh \
+	--prefix=/usr \
+	--disable-transparency \
+	--disable-light \
+	--disable-darker \
+	--disable-cinnamon \
+	--disable-gnome-shell \
+	--disable-metacity \
+	--disable-plank \
+	--disable-unity \
+	--disable-xfwm
 make install
